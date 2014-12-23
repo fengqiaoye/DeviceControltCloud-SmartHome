9ec2728fb07dddf531e452dcc697a401917518a3
richard <richard@cooxm.com>
2014-12-23 09:36:54 +0800

    2014-12-23

---
+++ DeviceControl/src/util/test.java
@@ -0,0 +1,14 @@
+package util;
+/** 
+ * @author Chen Guanghua E-mail: richard@cooxm.com
+ * @version Created£º23 Dec 2014 09:31:54 
+ */
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
